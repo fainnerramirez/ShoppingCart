767e0c5ad15231c889cd2463053b0dbf9a77b5f0
fainnerramirez <fainnerramirez@gmail.com>
2021-11-16 00:59:49 -0500

    add files change footer

--- src/components/Footer.component.tsx
+++ src/components/Footer.component.tsx
@@ -1,9 +1,8 @@
 import { Box, Text, useColorModeValue } from "@chakra-ui/react";
-import * as COLOR from "../utils/Colors";
 
 const Footer: React.FC = () => {
   return (
-    <Box bg={useColorModeValue("gray.50", COLOR.darkMin)} mt={20}>
+    <Box mt={20}>
       <Box py={7}>
         <Text pt={6} fontSize={"sm"} textAlign={"center"}>
           © 2021 Fainner Ramírez | Carlos Borrego. Todos los derechos reservados
--- src/pages/Products.page.tsx
+++ src/pages/Products.page.tsx
@@ -1,12 +1,13 @@
-import { Container, Divider } from "@chakra-ui/react";
+import { Container, Divider, useColorModeValue } from "@chakra-ui/react";
 import Categorys from "../components/Categorys.component";
 import Products from "../components/Products.component";
 
 const ProductsPage: React.FC = () => {
+  const colorDivider = useColorModeValue("gray.400", "gray.300");
   return (
     <Container maxW="container.xl" p={0}>
       <Categorys />
-      <Divider />
+      <Divider borderColor={colorDivider} />
       <Products />
     </Container>
   );
